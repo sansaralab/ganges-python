c270a8a2e97f568c4dde4b676071bf205f62c9de
Enapiuz <enapiuz@gmail.com>
2016-08-10 01:05:32 +0300

    command data types

--- ganges/types.py
+++ ganges/types.py
@@ -2,13 +2,13 @@ import ctypes
 
 
 COMMAND_TYPE = {
-    'PUSH': ctypes.c_bool(False),
-    'POP': ctypes.c_bool(True)
+    'PUSH': ctypes.c_uint8(0),
+    'POP': ctypes.c_uint8(1)
 }
 
 
 class Command:
     def __init__(self, command_type, data=''):
         self.command_type = command_type
-        self.data_length = len(str(data))
+        self.data_length = ctypes.c_uint32(len(str(data)))
         self.data = str(data)
